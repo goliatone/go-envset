0fa8c33a77c808a5f444a3036066395d7792736a
goliatone <burgosemi@gmail.com>
2016-06-17 18:42:43 -0400

    wip

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 
 	"github.com/vaughan0/go-ini"
 )
 
+func init() {
+	//we should load .envsetrc
+
+	_, filename, _, _ := runtime.Caller(1)
+	dirname := filepath.Dir(filename)
+	//recursively walk directory structure upward, trying to
+	//find our file until we reach root
+	fmt.Println(dirname)
+	fmt.Println(filepath.Clean(dirname + "/.."))
+}
+
 func main() {
 
+	//This should recursively try to find an envset file
+	//from here upwards.
 	env, err := ini.LoadFile(".envset")
 	if err != nil {
 		fmt.Println("Error parsing envset")
