40fb3a4246dd73f75f40d437818b544cb2aff0e2
goliatone <burgosemi@gmail.com>
2020-08-21 19:06:56 -0700

    update base implementation

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"runtime"
+	"strings"
+	"text/template"
+
+	// "path/filepath"
+	// "runtime"
 
 	"github.com/vaughan0/go-ini"
 )
@@ -16,31 +20,33 @@ func init() {
 	//TODO: we need to get this from envsetrc
 	//use https://github.com/jinzhu/configor
 	environments = []string{"development", "production", "staging", "testing", "local"}
+	/*
+		//Get the path to the current script
+		_, filename, _, _ := runtime.Caller(0)
+		//get the path from the script
+		dirname := filepath.Dir(filename)
 
-	_, filename, _, _ := runtime.Caller(1)
-	dirname := filepath.Dir(filename)
-
-	//recursively walk directory structure upward, trying to
-	//find our file until we reach root
-	for dirname != "/" {
-		dirname = filepath.Clean(dirname + "/..")
-		filename = filepath.Join(dirname, ".envsetrc")
-
-		config, err := ini.LoadFile(filename)
-		if err == nil {
-			envs := config["environments"]
-			fmt.Printf("config: %q\n", envs)
+		//recursively walk directory structure upward, trying to
+		//find our file until we reach root
+		for dirname != "/" {
+			filename = filepath.Join(dirname, ".envsetrc")
 			fmt.Printf("path: %s\n", filename)
-			// environments = config.Get("environment")
-			break
-		}
-	}
+			config, err := ini.LoadFile(filename)
+			if err == nil {
+				// envs := config["environments"]
+				// fmt.Printf("config: %q\n", config)
+				// fmt.Printf("envs: %q\n", envs)
+				break
+			}
+
+			dirname = filepath.Clean(dirname + "/..")
+		}*/
 }
 
 func main() {
 	//Show help
 	if len(os.Args) == 1 {
-		showHelpMessage()
+		showHelpMessage(os.Args[0])
 		os.Exit(0)
 	}
 	//This should recursively try to find an envset file
@@ -52,9 +58,23 @@ func main() {
 	}
 
 	found := false
+	//os.Args[0] is the main command `envset`
+	//os.Args[1] is the environment
 	environment := os.Args[1]
-	vars := make([]string, 0)
 
+	switch environment {
+	case "sync":
+		syncDemoFile()
+		os.Exit(0)
+		break
+	case "help":
+		showHelpMessage(os.Args[0])
+		os.Exit(0)
+		break
+	}
+
+	vars := make([]string, 0)
+	var tplVars map[string]string
 	for name, section := range env {
 		if name == environment {
 			if !validEnvironment(environment, environments) {
@@ -63,13 +83,15 @@ func main() {
 			}
 
 			found = true
+			tplVars = map[string]string{}
 			for key, value := range section {
 				// os.Setenv(key, value)
 				vars = append(vars, fmt.Sprintf("%s=%s", key, value))
-				// vars[key] = value
+				tplVars[key] = value
 			}
 		}
 	}
+
 	if found == false {
 		fmt.Printf("Error, environment %q not found.\n", environment)
 		os.Exit(1)
@@ -83,11 +105,49 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:]
 
+	//TODO: Run args string on replacement function so that we can replace ${MY_VAR}s
+	for i, arg := range args {
+		fmt.Printf("arg: %s\n", arg)
+		args[i] = interpolate(arg, tplVars)
+	}
+
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = vars
 	cmd.Run()
+}
+
+func interpolate(str string, vars map[string]string) string {
+	s := strings.Replace(str, "${", "${.", -1)
+	tmpl, err := template.New(str).Option("missingkey=error").Delims("${", "}").Parse(s)
+	if err != nil {
+		fmt.Printf("Error parsing command arguments: %+v", err)
+		os.Exit(1)
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, vars)
+	if err != nil {
+		//if strict we should exit
+		//else return str
+		if false {
+			fmt.Printf("Error parsing command arguments: %+v", err)
+			os.Exit(1)
+		} else {
+			return str
+		}
+	}
+	if buf.Len() == 0 {
+		return str
+	}
+	out := buf.String()
+	fmt.Printf("replaced to: %s len %d\n", out, buf.Len())
+
+	if out == "<no value>" {
+		return str
+	}
+	return out
 }
 
 func validEnvironment(env string, list []string) bool {
@@ -109,6 +169,40 @@ func showCurrentEnvironment(vars []string) {
 	}
 }
 
-func showHelpMessage() {
-	fmt.Println("envset: the environment management friend")
+func showHelpMessage(progname string) {
+	fmt.Printf("%s: the environment management friend\n", progname)
+	usage := `
+envset --help           => Usage()
+envset                  => ListEnvs()
+envset [environment]    => ShowEnv()
+envset [environment] -- [cmd] <args>   => ExecuteCommand()
+envset init             => InitializeProject()
+envset sync             => SyncProject()
+envset patch create     => PatchCreate()
+envset patch submit     => PatchSubmit()
+envset patch apply      => PatchApply()
+
+Configuration options:
+* name for template file default to envset.tpl
+* filepath for envset.tpl
+
+
+* How to watch a file and trigger a function on change
+* How to create a git hook in go? or bash?
+
+
+git hooks:
+* before commit: 
+    * Check if local envset file has changed, if so check to see if template new values were filled with data
+* on checkout:
+    * Check to see if we have an environment for this branch?
+    * Check to see if remote source has changed
+
+Look at [git-lfs](https://github.com/git-lfs/git-lfs) to see how they extend git with a new lfs subcommand so that we can have git env track
+	`
+	fmt.Printf("%s\n", usage)
+}
+
+func syncDemoFile() {
+
 }
