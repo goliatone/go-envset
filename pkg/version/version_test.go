afa1eff36467ba1f0098680281c887e01eae1211
goliatone <burgosemi@gmail.com>
2021-03-20 21:21:11 -0700

    add test

---
+++ pkg/version/version_test.go
@@ -0,0 +1,22 @@
+package version 
+
+import (
+	"testing"
+)
+
+func Test_GetVersion(t *testing.T) {
+	Tag = "0.0.0"
+	Time = "Time"
+	User = "username"
+
+	expected := "0.0.0-Time:username"
+
+	result := GetVersion()
+	if result == "" {
+		t.Errorf("GetVersion failed, expected %v got empty value", expected)
+	}
+
+	if result != expected {
+		t.Errorf("GetVersion failed, expected %v got %v", expected, result)
+	}
+}
